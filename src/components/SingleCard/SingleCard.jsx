7875ed808e43e6121a4752e90cd72867b20232c1
unknown <mehedytouhidurzaman@gmail.com>
2023-09-17 22:25:03 +0600

    feature added in card

--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 const SingleCard = (props) => {
     console.log(props);
-    const {image, features, name, published_in} = props.singleData
+    const {image, features, name, published_in} = props.singleData;
+    console.log(features);
 
     return (
         <div>
             <div className="card w-full bg-base-100 shadow-2xl">
                 <figure><img src={image} alt="No image found" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
+                    <h2 className="card-title">Features</h2>
+                    {
+                        features.map((feature,index) =>(
+                            <p>
+                                {index + 1}. {feature}
+                            </p>
+                        ))
+                    }
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
