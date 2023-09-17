7ab356a10a13be4c091e0e507417ae412ba9910b
unknown <mehedytouhidurzaman@gmail.com>
2023-09-17 14:25:57 +0600

    header & show card complete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,12 +13,11 @@ const Card = () => {
     },[])
     return (
         <>
-            {
-                data.map(singleData =>{
-                    console.log(singleData);
-                    return <SingleCard />
-                })
-            }
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-6 px-6'>
+                {
+                    data.map(singleData => <SingleCard singleData={singleData} key={singleData.id}></SingleCard>)
+                }
+            </div>
         </>
     );
 };
--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const SingleCard = (props) => {
+    console.log(props);
+    const {image, features, name, published_in} = props.singleData
+
     return (
         <div>
-            <h1>Hello</h1>
+            <div className="card w-full bg-base-100 shadow-2xl">
+                <figure><img src={image} alt="No image found" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
